c152ed3e0b7d401002ac6e89ecd8b87fa7613c41
Pranav904 <pranavnv14@gmail.com>
2024-10-26 17:00:34 +0530

    Refactor NewsCards component to include fetchedTags prop

--- briefly/src/components/NewsCards.jsx
+++ briefly/src/components/NewsCards.jsx
@@ -1,10 +1,11 @@
 import ThemeToggle from "./ThemeToggle";
 
-export default function NewsCards() {
+export default function NewsCards({ fetchedTags }) {
   return (
     <div className="flex flex-1">
       <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
         <div className="flex justify-center">
+        <p>{fetchedTags}</p>
           <ThemeToggle />
         </div>
         <div className="flex-column gap-2 flex-1">
