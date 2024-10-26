d4e3372ce5687043b41cd18df11949646016d766
Pranav904 <pranavnv14@gmail.com>
2024-10-26 15:45:17 +0530

    Refactor LogInSignUpButton component to update anchor tag styling and add whitespace handling

--- briefly/src/components/LogInSignUpButton.jsx
+++ briefly/src/components/LogInSignUpButton.jsx
@@ -2,8 +2,13 @@ import Link from "next/link";
 
 export default function LogInSignUpButton() {
   return (
-      <button className="mr-8 shadow-[0_4px_14px_0_rgb(0,0,0,10%)] hover:shadow-[0_6px_20px_rgba(93,93,93,23%)] px-8 py-2 bg-[#fff] rounded-md font-bold transition duration-200 ease-linear transform hover:-translate-y-1 transition duration-400">
-        <a href="/api/auth/login">Log In / Sign Up</a>
-      </button>
+    <button className="mr-8 shadow-[0_4px_14px_0_rgb(0,0,0,10%)] hover:shadow-[0_6px_20px_rgba(93,93,93,23%)] px-8 py-2 bg-[#fff] rounded-md font-bold transition duration-200 ease-linear transform hover:-translate-y-1 transition duration-400">
+      <a
+        href="/api/auth/login"
+        className="text-neutral-700 whitespace-pre inline-block !p-0 !m-0"
+      >
+        Log In / Sign Up
+      </a>
+    </button>
   );
 }
