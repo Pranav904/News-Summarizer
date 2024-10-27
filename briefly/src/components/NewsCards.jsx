e7e1b52929dd1fb40618eb9778a863af3c2f46d5
Pranav904 <pranavnv14@gmail.com>
2024-10-27 22:33:26 +0530

    Refactor NewsCards component to include selectedTags prop for fetching recommendations

--- briefly/src/components/NewsCards.jsx
+++ briefly/src/components/NewsCards.jsx
@@ -7,7 +7,7 @@ const alexBrush = Alex_Brush({
   subsets: ["latin"],
 });
 
-export default function NewsCards({ userProfile }) {
+export default function NewsCards({ userProfile, selectedTags }) {
   return (
     <div className="flex flex-1">
       <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
@@ -20,7 +20,7 @@ export default function NewsCards({ userProfile }) {
         <p className={`${alexBrush.className} text-3xl`}>Welcome , {userProfile.nickname} !</p>
           <ThemeToggle />
         </div>
-        <Recommendations/>
+        <Recommendations selectedTags={selectedTags}/>
       </div>
     </div>
   );
