47286681acb2365bbfc0f314aec596070f8985bd
Pranav904 <pranavnv14@gmail.com>
2024-10-27 15:06:30 +0530

    Refactor NewsCards component to include user profile and add Recommendations component

--- briefly/src/components/NewsCards.jsx
+++ briefly/src/components/NewsCards.jsx
@@ -1,15 +1,16 @@
 import ThemeToggle from "./ThemeToggle";
+import Recommendations from "./Recommendations";
 
-export default function NewsCards({ fetchedTags }) {
+export default function NewsCards({ userProfile }) {
   return (
     <div className="flex flex-1">
       <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
         <div className="flex justify-center">
-        <p>{fetchedTags}</p>
+        <p>Welcome, {userProfile.nickname} !</p>
           <ThemeToggle />
         </div>
-        <div className="flex-column gap-2 flex-1">
-          <div className="h-full w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"></div>
+        <div className="container mx-auto p-6 min-h-screen">
+          <Recommendations/>
         </div>
       </div>
     </div>
