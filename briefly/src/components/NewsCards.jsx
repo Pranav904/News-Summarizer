d8f86fb113e4b18a554010adf63dee48d8716ec6
Pranav904 <pranavnv14@gmail.com>
2024-10-28 19:00:29 +0530

    Refactor NewsCards component: Remove ThemeToggle

--- briefly/src/components/NewsCards.jsx
+++ briefly/src/components/NewsCards.jsx
@@ -12,15 +12,15 @@ export default function NewsCards({ userProfile, selectedTags }) {
     <div className="flex flex-1">
       <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
         <div className="flex justify-between">
-        <h1
-          className={`${alexBrush.className} invisible md:visible text-5xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50`}
-        >
-          Briefly
-        </h1>
-        {/* <p className={`${alexBrush.className} invisible text-2xl sm:visible`}>Welcome !</p> */}
-          <ThemeToggle />
+          <h1
+            className={`${alexBrush.className} invisible md:visible text-5xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50`}
+          >
+            Briefly
+          </h1>
+          {/* <p className={`${alexBrush.className} invisible text-2xl sm:visible`}>Welcome !</p> */}
+          {/* <ThemeToggle /> */}
         </div>
-        <Recommendations selectedTags={selectedTags}/>
+        <Recommendations selectedTags={selectedTags} />
       </div>
     </div>
   );
