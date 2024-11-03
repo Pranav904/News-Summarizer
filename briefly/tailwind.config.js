61414c10be076611d09c6f77f84b2d9ae253b5a4
Pranav904 <pranavnv14@gmail.com>
2024-11-03 20:50:12 +0530

    Add 3D card component, input component, and label component; update tabler icons dependency; enhance Tailwind CSS animations

--- briefly/tailwind.config.js
+++ briefly/tailwind.config.js
@@ -92,6 +92,17 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         spotlight: "spotlight 2s ease .75s 1 forwards",
+        shimmer: "shimmer 2s linear infinite",
+      },
+      keyframes: {
+        shimmer: {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
+          },
+        },
       },
     },
   },
