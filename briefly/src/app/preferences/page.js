c4c4be9dd73c56b51049e2c8edb3d2de38a9d753
Pranav904 <pranavnv14@gmail.com>
2024-11-09 18:30:10 +0530

    Update layout styling in PreferencesPage and ProfilePage for improved responsiveness in mobile.

--- briefly/src/app/preferences/page.js
+++ briefly/src/app/preferences/page.js
@@ -20,7 +20,7 @@ export default async function PreferencesPage() {
       )}
     >
       <SidebarComponent userProfile={user} />
-      <div className="rounded-l-2xl w-full dark:bg-black bg-white dark:bg-grid-white/[0.1] bg-grid-black/[0.1] relative flex items-center justify-center">
+      <div className="h-screen rounded-l-2xl w-full dark:bg-black bg-white dark:bg-grid-white/[0.1] bg-grid-black/[0.1] relative flex items-center justify-center">
         <div className="rounded-l-2xl absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <PreferencesComponent user={session.user} />
       </div>
--- briefly/src/app/profile/page.js
+++ briefly/src/app/profile/page.js
@@ -21,8 +21,8 @@ export default async function ProfilePage() {
       )}
     >
       <SidebarComponent userProfile={user} />
-      <div className="rounded-l-2xl w-full dark:bg-black bg-white dark:bg-grid-white/[0.1] bg-grid-black/[0.1] relative flex items-center justify-center">
-        <div className="rounded-l-2xl absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <div className="h-screen rounded-t-2xl md:rounded-l-2xl w-full dark:bg-black bg-white dark:bg-grid-white/[0.1] bg-grid-black/[0.1] relative flex items-center justify-center">
+        <div className="rounded-t-2xl md:rounded-l-2xl absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         <div className="flex flex-col items-center">
           <Image
             src={user.picture}
